8d3658304390286d62249f1fb8377662b101757b
dn24002 <dn24002@edu.lu.lv>
2025-08-09 07:59:35 +0000

    changes #2

--- Sub_Strings.rb
+++ Sub_Strings.rb
@@ -1,16 +1,12 @@
-def find_the_word(word, dictionary)
-  len = word.length
-  
-  (0...len).each do |i| 
-    (i+1..len).each do |j| 
-      substring = word[i, j-i]  # Corrected slicing
-      if dictionary.include?(substring)
-        puts "'#{substring}' is found in the dictionary!"
-      end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "go"]
+word = "go"
+def find_word(word, dictionary)
+    if dictionary.include?(word)
+      puts "yesss!"
+      count = dictionary.count(word)
+      puts "#{word} : #{count}"
+    else 
+      puts "noo"
     end
-  end
-end   
-
-
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-find_the_word("below",dictionary )
+end
+puts find_word(word, dictionary)
